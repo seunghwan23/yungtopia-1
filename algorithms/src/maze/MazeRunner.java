ec7dc2cd8c582214f6b8ee92556d9033c19dc8c8
pureugong <pureugong@gmail.com>
2015-04-01 22:28:14 +0900

    add maze test file

--- algorithms/src/maze/MazeRunner.java
+++ algorithms/src/maze/MazeRunner.java
@@ -1,7 +1,176 @@
 package maze;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MazeRunner {
-	public static void main(String[] args) {
-		System.out.println("yungtopia!!");
+
+	public int[][] MAZE;
+	public int SIZE;
+	public int[] currentLocation;
+	public List<int[]> tracks;
+
+	public MazeRunner() {
+		this.MAZE = new int[][] { { 0, 1, 1, 1, 1 }, { 1, 0, 1, 1, 1 },
+				{ 0, 1, 1, 1, 1 }, { 0, 1, 1, 1, 1 }, { 0, 0, 0, 0, 0 } };
+		this.SIZE = this.MAZE.length - 1;
+		this.currentLocation = new int[] { 0, 0 };
+		tracks = new ArrayList<int[]>();
+
 	}
+
+	public int[] up(int[] location) {
+		if (location == null)
+			return null;
+		if (0 < location[0]) {
+			location[0] -= 1;
+			return location;
+		} else {
+			return null;
+		}
+	}
+
+	public int[] down(int[] location) {
+		if (location == null)
+			return null;
+		if (location[0] < this.SIZE) {
+			location[0] += 1;
+			return location;
+		} else {
+			return null;
+		}
+	}
+
+	public int[] left(int[] location) {
+		if (location == null)
+			return null;
+		if (0 < location[1]) {
+			location[1] -= 1;
+			return location;
+		} else {
+			return null;
+		}
+	}
+
+	public int[] right(int[] location) {
+		if (location == null)
+			return null;
+		if (location[1] < this.SIZE) {
+			location[1] += 1;
+			return location;
+		} else {
+			return null;
+		}
+	}
+
+	public void moveUp() {
+		markHere();
+		currentLocation[0] -= 1;
+		checkLocation();
+	}
+
+	public void moveDown() {
+		markHere();
+		currentLocation[0] += 1;
+		checkLocation();
+	}
+
+	public void moveLeft() {
+		markHere();
+		currentLocation[1] -= 1;
+		checkLocation();
+	}
+
+	public void moveRight() {
+		markHere();
+		currentLocation[1] += 1;
+		checkLocation();
+	}
+
+	private void moveRightUp() {
+		markHere();
+		currentLocation[1] += 1;
+		currentLocation[0] -= 1;
+		checkLocation();
+	}
+
+	private void moveLeftUp() {
+		markHere();
+		currentLocation[1] -= 1;
+		currentLocation[0] -= 1;
+		checkLocation();
+	}
+
+	private void moveLeftDown() {
+		markHere();
+		currentLocation[1] -= 1;
+		currentLocation[0] += 1;
+		checkLocation();
+	}
+
+	private void moveRightDown() {
+		markHere();
+		currentLocation[1] += 1;
+		currentLocation[0] += 1;
+		checkLocation();
+	}
+
+	public void escape() {
+		checkLocation();
+		while (true) {
+
+			while (possible(right(currentLocation())))
+				moveRight();
+
+			while (possible(down(right(currentLocation()))))
+				moveRightDown();
+
+			while (possible(down(currentLocation())))
+				moveDown();
+
+			while (possible(left(down(currentLocation()))))
+				moveLeftDown();
+
+			while (possible(left(currentLocation())))
+				moveLeft();
+
+			while (possible(up(left(currentLocation()))))
+				moveLeftUp();
+
+			while (possible(up(currentLocation())))
+				moveUp();
+
+			while (possible(right(up(currentLocation()))))
+				moveRightUp();
+
+			if (currentLocation()[0] == this.SIZE
+					&& currentLocation()[1] == this.SIZE)
+				break;
+		}
+	}
+
+	public void checkLocation() {
+		// tracks.add(currentLocation());
+		System.out.print(currentLocation[0] + "" + currentLocation[1] + " ");
+	}
+
+	private void markHere() {
+		this.MAZE[currentLocation[0]][currentLocation[1]] = 2;
+	}
+
+	private int[] currentLocation() {
+		return currentLocation.clone();
+	}
+
+	private boolean possible(int[] location) {
+		boolean flag = false;
+		if (location != null && valueOfLocation(location) == 0)
+			flag = true;
+		return flag;
+	}
+
+	private int valueOfLocation(int[] location) {
+		return MAZE[location[0]][location[1]];
+	}
+
 }
---
+++ algorithms/src/maze/MazeSpec.java
@@ -0,0 +1,11 @@
+package maze;
+
+public class MazeSpec {
+	public static void main(String[] args) {
+		// MazeRunner init
+		MazeRunner runner = new MazeRunner();
+		
+		//try escape!!
+		runner.escape();
+	}
+}
